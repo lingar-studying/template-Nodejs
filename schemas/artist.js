bc53fca1cc79afe76c7e5c9b20a2a275dc8371e8
shay shalom izhak <alonizhakalonizhak@gmail.com>
2025-06-18 20:42:47 +0300

    added a new update

--- schemas/artist.js
+++ schemas/artist.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const artistSchema = new mongoose.Schema({
   name: { type: String, required: true },
   categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  location: { type: String }, // מיקום
+  lifeApproach: { type: String }, // תיאור של גישת חיים
+  likes: { type: String }, // מה הוא אוהב
 });
 
 module.exports = { artistSchema };
